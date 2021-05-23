e8be156bd998a3bfce6e2980dbd867b5c29d237e
gosiakab <61922732+gosiakab@users.noreply.github.com>
2021-05-23 19:12:40 +0100

    Update settings.js

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,49 +1,50 @@
+/* global Handlebars */
 export const select = {
   templateOf: {
-    menuProduct: '#template-menu-product',
-    cartProduct: '#template-cart-product', // CODE ADDED
+    menuProduct: "#template-menu-product",
+    cartProduct: "#template-cart-product", // CODE ADDED
   },
   containerOf: {
-    menu: '#product-list',
-    cart: '#cart',
+    menu: "#product-list",
+    cart: "#cart",
   },
   all: {
-    menuProducts: '#product-list > .product',
-    menuProductsActive: '#product-list > .product.active',
-    formInputs: 'input, select',
+    menuProducts: "#product-list > .product",
+    menuProductsActive: "#product-list > .product.active",
+    formInputs: "input, select",
   },
   menuProduct: {
-    clickable: '.product__header',
-    form: '.product__order',
-    priceElem: '.product__total-price .price',
-    imageWrapper: '.product__images',
-    amountWidget: '.widget-amount',
+    clickable: ".product__header",
+    form: ".product__order",
+    priceElem: ".product__total-price .price",
+    imageWrapper: ".product__images",
+    amountWidget: ".widget-amount",
     cartButton: '[href="#add-to-cart"]',
   },
   widgets: {
     amount: {
-      input: 'input.amount', // CODE CHANGED
+      input: "input.amount", // CODE CHANGED
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
     },
   },
   // CODE ADDED START
   cart: {
-    productList: '.cart__order-summary',
-    toggleTrigger: '.cart__summary',
+    productList: ".cart__order-summary",
+    toggleTrigger: ".cart__summary",
     totalNumber: `.cart__total-number`,
     totalPrice:
-      '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
-    subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
-    deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
-    form: '.cart__order',
+      ".cart__total-price strong, .cart__order-total .cart__order-price-sum strong",
+    subtotalPrice: ".cart__order-subtotal .cart__order-price-sum strong",
+    deliveryFee: ".cart__order-delivery .cart__order-price-sum strong",
+    form: ".cart__order",
     formSubmit: '.cart__order [type="submit"]',
     phone: '[name="phone"]',
     address: '[name="address"]',
   },
   cartProduct: {
-    amountWidget: '.widget-amount',
-    price: '.cart__product-price',
+    amountWidget: ".widget-amount",
+    price: ".cart__product-price",
     edit: '[href="#edit"]',
     remove: '[href="#remove"]',
   },
@@ -52,12 +53,12 @@ export const select = {
 
 export const classNames = {
   menuProduct: {
-    wrapperActive: 'active',
-    imageVisible: 'active',
+    wrapperActive: "active",
+    imageVisible: "active",
   },
   // CODE ADDED START
   cart: {
-    wrapperActive: 'active',
+    wrapperActive: "active",
   },
   // CODE ADDED END
 };
@@ -74,9 +75,9 @@ export const settings = {
   },
   // CODE ADDED END
   db: {
-    url: '//localhost:3131',
-    product: 'product',
-    order: 'order',
+    url: "//localhost:3131",
+    product: "product",
+    order: "order",
   },
 };
 
