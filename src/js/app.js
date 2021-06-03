321e1ee107aadfcad68deda7c44440086900e9cb
gosiakab <61922732+gosiakab@users.noreply.github.com>
2021-06-03 09:03:21 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,6 @@
 import { settings, select, classNames, templates } from "./settings.js";
 import Product from "./components/Product.js";
 import Cart from "./components/Cart.js";
-/* import AmountWidget from "./components/AmountWidget.js";
-import CartProduct from "./components/CartProduct.js";*/
 
 const app = {
   initData: function () {
