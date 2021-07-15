4c9395fa47bd589a7f386120c031516d90667e2f
gosiakab <61922732+gosiakab@users.noreply.github.com>
2021-07-15 21:14:31 +0100

    Update settings.js

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,90 +1,144 @@
-/* global Handlebars, dataSource */
+/* global Handlebars */
+export const select = {
+  templateOf: {
+    menuProduct: '#template-menu-product',
+    cartProduct: '#template-cart-product', // CODE ADDED
+    bookingWidget: '#template-booking-widget',
+  },
+  containerOf: {
+    menu: '#product-list',
+    cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper',
+  },
+  all: {
+    menuProducts: '#product-list > .product',
+    menuProductsActive: '#product-list > .product.active',
+    formInputs: 'input, select',
+  },
+  menuProduct: {
+    clickable: '.product__header',
+    form: '.product__order',
+    priceElem: '.product__total-price .price',
+    imageWrapper: '.product__images',
+    amountWidget: '.widget-amount',
+    cartButton: '[href="#add-to-cart"]',
+  },
+  widgets: {
+    amount: {
+      input: 'input.amount', // CODE CHANGED
+      linkDecrease: 'a[href="#less"]',
+      linkIncrease: 'a[href="#more"]',
+    },
+    datePicker: {
+      wrapper: '.date-picker',
+      input: `input[name="date"]`,
+    },
+    hourPicker: {
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output',
+    },
+  },
+  // CODE ADDED START
+  cart: {
+    productList: '.cart__order-summary',
+    toggleTrigger: '.cart__summary',
+    totalNumber: `.cart__total-number`,
+    totalPrice:
+      '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+    subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+    deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+    form: '.cart__order',
+    formSubmit: '.cart__order [type="submit"]',
+    phone: '[name="phone"]',
+    address: '[name="address"]',
+  },
+  cartProduct: {
+    amountWidget: '.widget-amount',
+    price: '.cart__product-price',
+    edit: '[href="#edit"]',
+    remove: '[href="#remove"]',
+  },
+  booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
 
-export const utils = {}; // eslint-disable-line no-unused-vars
-
-utils.createDOMFromHTML = function (htmlString) {
-  let div = document.createElement("div");
-  div.innerHTML = htmlString.trim();
-  return div.firstChild;
+  // CODE ADDED END
 };
 
-utils.createPropIfUndefined = function (obj, key, value = []) {
-  if (!obj.hasOwnProperty(key)) {
-    obj[key] = value;
-  }
+export const classNames = {
+  menuProduct: {
+    wrapperActive: 'active',
+    imageVisible: 'active',
+  },
+  // CODE ADDED START
+  cart: {
+    wrapperActive: 'active',
+  },
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
+
+  // CODE ADDED END
 };
 
-utils.serializeFormToObject = function (form) {
-  let output = {};
-  if (typeof form == "object" && form.nodeName == "FORM") {
-    for (let field of form.elements) {
-      if (
-        field.name &&
-        !field.disabled &&
-        field.type != "file" &&
-        field.type != "reset" &&
-        field.type != "submit" &&
-        field.type != "button"
-      ) {
-        if (field.type == "select-multiple") {
-          for (let option of field.options) {
-            if (option.selected) {
-              utils.createPropIfUndefined(output, field.name);
-              output[field.name].push(option.value);
-            }
-          }
-        } else if (
-          (field.type != "checkbox" && field.type != "radio") ||
-          field.checked
-        ) {
-          utils.createPropIfUndefined(output, field.name);
-          output[field.name].push(field.value);
-        } else if (!output[field.name]) output[field.name] = [];
-      }
-    }
-  }
-  return output;
-};
-utils.queryParams = function (params) {
-  return Object.keys(params)
-    .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(params[k]))
-    .join("&");
+export const settings = {
+  hours: {
+    open: 12,
+    close: 24,
+  },
+  amountWidget: {
+    defaultValue: 1,
+    defaultMin: 1,
+    defaultMax: 9,
+  }, // CODE CHANGED
+  // CODE ADDED START
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
+  cart: {
+    defaultDeliveryFee: 20,
+  },
+  booking: {
+    tableIdAttribute: 'data-table',
+  },
+  // CODE ADDED END
+  db: {
+    url: '//localhost:3131',
+    product: 'product',
+    order: 'order',
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
+  },
 };
 
-utils.convertDataSourceToDbJson = function () {
-  const productJson = [];
-  for (let key in dataSource.products) {
-    productJson.push(Object.assign({ id: key }, dataSource.products[key]));
-  }
-
-  console.log(JSON.stringify({ product: productJson, order: [] }, null, "  "));
+export const templates = {
+  menuProduct: Handlebars.compile(
+    document.querySelector(select.templateOf.menuProduct).innerHTML
+  ),
+  // CODE ADDED START
+  cartProduct: Handlebars.compile(
+    document.querySelector(select.templateOf.cartProduct).innerHTML
+  ),
+  bookingWidget: Handlebars.compile(
+    document.querySelector(select.templateOf.bookingWidget).innerHTML
+  ),
+  // CODE ADDED END
 };
-utils.numberToHour = function (number) {
-  return (
-    (Math.floor(number) % 24) + ":" + ((number % 1) * 60 + "").padStart(2, "0")
-  );
-};
-
-utils.hourToNumber = function (hour) {
-  const parts = hour.split(":");
-
-  return parseInt(parts[0]) + parseInt(parts[1]) / 60;
-};
-
-utils.dateToStr = function (dateObj) {
-  return dateObj.toISOString().slice(0, 10);
-};
-
-utils.addDays = function (dateStr, days) {
-  const dateObj = new Date(dateStr);
-  dateObj.setDate(dateObj.getDate() + days);
-  return dateObj;
-};
-
-Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
-  return arg1 == arg2 ? options.fn(this) : options.inverse(this);
-});
-
-Handlebars.registerHelper("joinValues", function (input, options) {
-  return Object.values(input).join(options.fn(this));
-});
