f02f947146883d3132880fb4908c8a6d692b9ae4
gosiakab <61922732+gosiakab@users.noreply.github.com>
2021-07-23 09:15:49 +0100

    Update Booking.js

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,6 @@
 import { templates, select } from "../settings.js";
 import DatePicker from "./DatePicker.js";
+import HourPicker from "./HourPicker.js";
 
 import AmountWidget from "./AmountWidget.js";
 
@@ -29,6 +30,9 @@ class Booking {
     thisBooking.dom.datePicker = element.querySelector(
       select.widgets.datePicker.wrapper
     );
+    thisBooking.dom.hourPicker = element.querySelector(
+      select.widgets.hourPicker.wrapper
+    );
   }
 
   initWidgets() {
@@ -37,6 +41,7 @@ class Booking {
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
 export default Booking;
